73a74cf15fe2c297097700400aa4789d37ce77e1
CHEX <52354962+SFCHEX@users.noreply.github.com>
2023-04-10 16:27:05 +0200

    basic data members for process

--- ProcessScheduler/Process.h
+++ ProcessScheduler/Process.h
@@ -2,9 +2,20 @@
 
 #include "ADT/Map.h"
 
+enum State
+{
+	NEW,
+	RDY,
+	RUN,
+	BLK,
+	TRM,
+	ORPH
+};
+
+
 class Process
 {
-public:
+private:
 	int PID, 
 		AT,
 		RT,
@@ -14,5 +25,25 @@ public:
 		WT;
 
 	Map IO_R_D;
+	State PS; // this is the process state variable. Process states are stored here
+
+public:
+	int get_PID();
+	int get_AT();
+	int get_RT();
+	int get_CT();
+	int get_TT();
+	int get_TRT();
+	int get_WT();
+	Map get_IO_R_D();
+	State get_State();
+
+	void set_TT(int);
+	void set_RT(int);
+	void set_TRT();
+	void set_WT();
+	void set_State(State);
+
+	Process(int pid, int at, int ct, Map io_r_d);
 };
 
