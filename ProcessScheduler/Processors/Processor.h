a4aeec9fd0d2c1e318317cae1a610b8a480eccaf
CHEX <52354962+SFCHEX@users.noreply.github.com>
2023-04-11 00:45:29 +0200

    merging main into saif

--- ProcessScheduler/Processors/Processor.h
+++ ProcessScheduler/Processors/Processor.h
@@ -1,8 +1,8 @@
 #pragma once
 
 #include "../ADT/LinkedList.h"
-#include "../ADT/Queue.h"
-#include "../ADT/Stack.h"
+#include "../ADT/Linked_Queue/Queue.h"
+#include "../ADT/Array_Stack/Stack.h"
 #include "../Process.h"
 
 class Processor
